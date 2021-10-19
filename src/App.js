4f673c38057918deff6d35e9693f4d305feb3710
Nabid Hasan <nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bdgit config --global user.email nabid15-1832@diu.edu.bd>
2021-10-19 22:19:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Banner from './Pages/Shared/Banner/Banner';
 import Header from './Pages/Shared/Header/Header';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
